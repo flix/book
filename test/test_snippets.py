a535d2a2000907db3010e5cafaea0a6647cce9df
Matthew Lutze <mlutze@users.noreply.github.com>
2024-12-20 11:01:45 +0100

    finish test thing

--- test/test_snippets.py
+++ test/test_snippets.py
@@ -4,11 +4,17 @@ import os
 import glob
 import tempfile
 import subprocess
+import shutil
 
 SRC_DIR = "src"
-SNIPPET_DIR = tempfile.TemporaryDirectory()
+_BUILD_DIR = tempfile.TemporaryDirectory("flix-test-snippets")
+BUILD_DIR = _BUILD_DIR.name
+SNIPPET_DIR = os.path.join(BUILD_DIR, "snippets")
+os.mkdir(SNIPPET_DIR)
+FLIX_PATH = os.path.abspath("flix.jar")
 
 # returns all the code snippets in the given file as a tuple: (content, language, line)
+# file_name is relative to the src directory
 def extract_code_snippets(file_name):
     snippets = []
 
@@ -40,22 +46,67 @@ def extract_code_snippets(file_name):
 
     return snippets
 
+def getSubject(file):
+    file_name = os.path.basename(file)
+    subject = os.path.splitext(file_name)[0]
+    return subject
+
 # make snippets files for the given file_path
+# file_name is relative to the src directory
 def makeSnippetFilesForFile(file_name):
     for (block, language, linenum) in extract_code_snippets(file_name):
         if "flix" in language and "ignore" not in language:
-            test_file = f"{file_name}_{linenum}.flix"
+            subject = getSubject(file_name)
+            test_file = f"{subject}_{linenum}.flix"
             with open(os.path.join(SNIPPET_DIR, test_file), "w") as file:
                 file.write(block)
 
 # make snippets for all the source files and put them in the snippet directory
-def makeSnippetFilesForDir():
+def makeSnippetFiles():
     files = glob.glob("*.md", root_dir=SRC_DIR)
     for file in files:
         makeSnippetFilesForFile(file)
 
+def flix(cmd, *, cwd):
+    return subprocess.run(f"java -jar {FLIX_PATH} {cmd}", cwd=cwd, shell=True)
 
-makeSnippetFilesForDir("/tmp/matttest/")
+# file_name is relative to the snippet directory
+def checkFlixFile(file_name):
+    # create a project for the file
+    subject = getSubject(file_name)
+    project_dir = os.path.join(BUILD_DIR, subject)
+    os.mkdir(project_dir)
+    flix("init", cwd=project_dir)
 
-def checkFlixFile(file):
-    subprocess.run("java -jar flix.jar check")
+    # delete the Main.flix file
+    main = os.path.join(project_dir, "src", "Main.flix")
+    os.remove(main)
+
+    # copy the flix file to the project
+    source = os.path.join(SNIPPET_DIR, file_name)
+    target = os.path.join(project_dir, "src", file_name)
+    shutil.copy(source, target)
+
+    # type-check the project
+    return flix("check --no-install", cwd=project_dir)
+
+def checkFlixFiles():
+    files = glob.glob("*.flix", root_dir=SNIPPET_DIR)
+    failures = []
+    for file in files:
+        result = checkFlixFile(file)
+        if result.returncode != 0:
+            failures.append(file)
+    return failures
+
+makeSnippetFiles()
+failures = checkFlixFiles()
+
+if failures != []:
+    print(f"Errors in {len(failures)} snippet(s).")
+    for failure in failures:
+        print(failure)
+    exit(1)
+else:
+    print("All snippets compiled successfully.")
+    exit(0)
